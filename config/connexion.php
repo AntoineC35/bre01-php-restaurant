e59d55bd0d3cf296448fef654e49d6d9d5b2b21e
AntoineC35 <antoine.cormier@3wa.io>
2024-01-22 09:25:32 +0000

    début du projet

---
+++ config/connexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "antoinecormier_favorite_resto";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
+
+$user = "antoinecormier";
+$password = "52ca4b4148c7bf06f35e89032a52940f";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
---
+++ layout.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Favorite Restaurant</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+              crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body class="container-fluid">
+
+    <?php
+
+    if($route === "connexion")
+    {
+        require "templates/connexion.phtml";
+    }
+    else if($route === "inscription")
+    {
+        require "templates/inscription.phtml";
+    }
+    else if($route === "recommanded")
+    {
+        require "templates/recommanded.phtml";
+    }
+    else
+    {
+        require "templates/list_resto.phtml";
+    }
+
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"></script>
+    </body>
+</html>
